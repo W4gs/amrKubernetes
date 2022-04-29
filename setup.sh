f8d02a38d30f23e440bda1bcabcd23b762866c25
Wags <1270341+W4gs@users.noreply.github.com>
2022-04-29 18:51:40 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-echo "Making $ASKDIR Directory...."
-/bin/mkdir -p $ASKDIR
+echo "Making $AMRDIR Directory...."
+/bin/mkdir -p $AMRDIR
 
-echo "Changing To $ASKDIR Directory...."
-/bin/cd $ASKDIR
+echo "Changing To $AMRDIR Directory...."
+/bin/cd $AMRDIR
 
 echo "Installing Dependencies...."
 /bin/yum install unzip dotnet-host dotnet-sdk-3.1 -y 2>&1 > /dev/nulll
 
 echo "Downloading Client...."
-/bin/curl https://static.askmrrobot.com/wowsite/client/$DLFILE -o $ASKDIR/$DLFILE 2>&1 > /dev/null
+/bin/curl https://static.askmrrobot.com/wowsite/client/$DLFILE -o $AMRDIR/$AMRFILE 2>&1 > /dev/null
 
 echo "Unzipping Client...."
-/bin/unzip $ASKDIR/$DLFILE -d $ASKDIR/ 2>&1 > /dev/nulll
+/bin/unzip $AMRDIR/$AMRFILE -d $AMRDIR/ 2>&1 > /dev/nulll
 
-echo "Writing out settings.json to $ASKDIR/AskMrRobotClient/...."
-echo -e '{\n  \"ClientUserSettings\": {\n    "Username": "$AMRUSERNAME",\n    "Token": "$AMRTOKEN",\n    "PerformanceMode": "Batch",\n    "Threads": 1.0,\n    "EnableGlobalNetwork": false,\n    "GlobalNetworkThreads": -1.0,\n    "GlobalNetworkSchedule": ""\n  }\n}\n" > $ASKDIR/AskMrRobotClient/settings.json
+echo "Writing out settings.json to $AMRDIR/AskMrRobotClient/...."
+echo -e '{\n  \"ClientUserSettings\": {\n    "Username": "$AMRUSERNAME",\n    "Token": "$AMRTOKEN",\n    "PerformanceMode": "Batch",\n    "Threads": 1.0,\n    "EnableGlobalNetwork": false,\n    "GlobalNetworkThreads": -1.0,\n    "GlobalNetworkSchedule": ""\n  }\n}\n" > $AMRDIR/AskMrRobotClient/settings.json
 
 echo "Running AMR Client...."
-/bin/dotnet $ASKDIR/AskMrRobotClient/simclient.dll
+/bin/dotnet $AMRDIR/AskMrRobotClient/simclient.dll
