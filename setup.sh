9607abcf0b9c9cc3e0b087373f453c93358a5ac7
Wags <1270341+W4gs@users.noreply.github.com>
2022-04-29 18:44:46 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ echo "Unzipping Client...."
 /bin/unzip $ASKDIR/$DLFILE -d $ASKDIR/ 2>&1 > /dev/nulll
 
 echo "Writing out settings.json to $ASKDIR/AskMrRobotClient/...."
-echo -e '{\n  \"ClientUserSettings\": {\n    "Username": "autris",\n    "Token": "ae8e4af3830c4175bd0c5e12e22a5848a16e783ac9ae443aba57f30130f1e37d",\n    "PerformanceMode": "Batch",\n    "Threads": 1.0,\n    "EnableGlobalNetwork": false,\n    "GlobalNetworkThreads": -1.0,\n    "GlobalNetworkSchedule": ""\n  }\n}\n" > $ASKDIR/AskMrRobotClient/settings.json
+echo -e '{\n  \"ClientUserSettings\": {\n    "Username": "$AMRUSERNAME",\n    "Token": "$AMRTOKEN",\n    "PerformanceMode": "Batch",\n    "Threads": 1.0,\n    "EnableGlobalNetwork": false,\n    "GlobalNetworkThreads": -1.0,\n    "GlobalNetworkSchedule": ""\n  }\n}\n" > $ASKDIR/AskMrRobotClient/settings.json
 
 
 echo "Running AMR Client...."
