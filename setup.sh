2b5690f76b90c3fd7dc473e6ba07f49c68122a43
Wags <1270341+W4gs@users.noreply.github.com>
2022-04-29 20:53:16 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-echo "Making $AMRDIR Directory...."
-/bin/mkdir -p $AMRDIR
-
 echo "Installing Dependencies...."
-/bin/yum install unzip dotnet-host dotnet-sdk-3.1 -y 2>&1 > /dev/nulll
+/bin/apt install unzip -y
 
 echo "Downloading Client...."
-/bin/curl https://static.askmrrobot.com/wowsite/client/$AMRFILE -o $AMRDIR/$AMRFILE 2>&1 > /dev/null
+/bin/curl https://static.askmrrobot.com/wowsite/client/$AMRFILE -o $AMRDIR/$AMRFILE
 
 echo "Unzipping Client...."
-/bin/unzip $AMRDIR/$AMRFILE -d $AMRDIR/ 2>&1 > /dev/nulll
+/bin/unzip $AMRDIR/$AMRFILE -d $AMRDIR/
 
 echo "Writing out settings.json to $AMRDIR/AskMrRobotClient/...."
 echo -e "{\n  \"ClientUserSettings\": {\n    \"Username\": \"$AMRUSERNAME\",\n    \"Token\": \"$AMRTOKEN\",\n    \"PerformanceMode\": \"Batch\",\n    \"Threads\": 1.0,\n    \"EnableGlobalNetwork\": false,\n    \"GlobalNetworkThreads\": -1.0,\n    \"GlobalNetworkSchedule\": \"\"\n  }\n}\n" > $AMRDIR/AskMrRobotClient/settings.json
